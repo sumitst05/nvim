e05a50466db3bb1e757c617e846f31601469c1fb
sumitst05 <sumitst05@gmail.com>
2024-02-12 14:39:23 +0530

    chore: suppressed multiple encoding warning

--- lua/sumit/plugins/lsp-config.lua
+++ lua/sumit/plugins/lsp-config.lua
@@ -1,61 +1,71 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bashls",
-					"clangd",
-					"gopls",
-					"html",
-					"jdtls",
-					"marksman",
-					"lua_ls",
-					"pyright",
-					"rust_analyzer",
-					"tsserver",
-					"tailwindcss",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "bashls",
+          "clangd",
+          "gopls",
+          "html",
+          "jdtls",
+          "marksman",
+          "lua_ls",
+          "pyright",
+          "rust_analyzer",
+          "tsserver",
+          "tailwindcss",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			local languages = {
-				"bashls",
-				"clangd",
-				"gopls",
-				"html",
-				"jdtls",
-				"marksman",
-				"lua_ls",
-				"ocamllsp",
-				"pyright",
-				"rust_analyzer",
-				"tsserver",
-				"tailwindcss",
-			}
+      local languages = {
+        "bashls",
+        "clangd",
+        "gopls",
+        "html",
+        "jdtls",
+        "marksman",
+        "lua_ls",
+        "ocamllsp",
+        "pyright",
+        "rust_analyzer",
+        "tsserver",
+        "tailwindcss",
+      }
 
-			for _, lang in ipairs(languages) do
-				lspconfig[lang].setup({
-					capabilities = capabilities,
-				})
-			end
+      for _, lang in ipairs(languages) do
+        if lang == "clangd" then
+          lspconfig[lang].setup({
+            capabilities = capabilities,
+            cmd = {
+              "clangd",
+              "--offset-encoding=utf-16",
+            },
+          })
+        else
+          lspconfig[lang].setup({
+            capabilities = capabilities,
+          })
+        end
+      end
 
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
-			vim.api.nvim_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true })
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { noremap = true })
-		end,
-	},
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
+      vim.api.nvim_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true })
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { noremap = true })
+    end,
+  },
 }
