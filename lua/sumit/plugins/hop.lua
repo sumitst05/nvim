6328f0fdaf6e6497504801e2cfd70627dc229cfa
sumitst05 <sumitst05@gmail.com>
2024-12-09 19:58:59 +0530

    added: hop.lua

---
+++ lua/sumit/plugins/hop.lua
@@ -0,0 +1,25 @@
+local M = {
+	'smoka7/hop.nvim',
+	version = "*",
+}
+
+function M.config()
+	require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+
+	local hop = require('hop')
+	local directions = require('hop.hint').HintDirection
+	vim.keymap.set('', 'f', function()
+		hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+	end, { remap = true })
+	vim.keymap.set('', 'F', function()
+		hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+	end, { remap = true })
+	vim.keymap.set('', 't', function()
+		hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+	end, { remap = true })
+	vim.keymap.set('', 'T', function()
+		hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+	end, { remap = true })
+end
+
+return M
--- lua/sumit/plugins/trouble.lua
+++ lua/sumit/plugins/trouble.lua
@@ -3,24 +3,8 @@ local M = {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VeryLazy",
 	opts = {
-		position = "right",
-		width = 50,
-		icons = true,
-		mode = "document_diagnostics",
-		action_keys = {
-			close = "q",
-			cancel = "<esc>",
-			refresh = "r",
-			toggle_mode = "m", -- toggle between document and workspace mode
-			previous = "k",
-			next = "j",
-		},
-
 		vim.keymap.set("n", "<leader>xx", function()
-			require("trouble").toggle()
-		end),
-		vim.keymap.set("n", "<leader>xr", function()
-			require("trouble").toggle("lsp_references")
+			vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics win = { type = split, position = right }<cr>")
 		end),
 	},
 }
